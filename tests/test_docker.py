84f6f1c3aaa6a3e30bfa9404b09631a4cdc5ec76
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-12 08:52:10 +0000

    Don't run docker tests on Windows or macOS

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -3,8 +3,27 @@ import sys
 
 # 3rd party
 import pytest
+from _pytest.mark import MarkDecorator
+from domdf_python_tools.testing import not_windows
 
 
+def not_mac(reason: str = "Not required on Windows.", ) -> MarkDecorator:
+	"""
+	Factory function to return a ``@pytest.mark.skipif`` decorator that will
+	skip a test if the current platform is macOS.
+
+	:param reason: The reason to display when skipping.
+
+	:rtype:
+
+	.. versionadded:: 0.9.0
+	"""  # noqa D400
+
+	return pytest.mark.skipif(condition=sys.platform == "darwin", reason=reason)
+
+
+@not_mac(reason="Docker does not work correctly on macOS.")
+@not_windows(reason="Docker does not work correctly on Windows.")
 def test_building(repo_root, docker_client):
 	if not (repo_root / "Dockerfile").is_file():
 		pytest.skip("Dockerfile not found.")
