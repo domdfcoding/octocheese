bb8f02a9a810e8c418dafbce979c0bd87fbdd65c
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-05-14 15:06:31 +0100

    Skip installing pytest_docker_tools on non-Linux

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+# stdlib
+from typing import Tuple
+
 # 3rd party
 import pytest
 from betamax import Betamax  # type: ignore
 from domdf_python_tools.paths import PathPlus
 
-pytest_plugins = ("pytest_docker_tools", "coincidence", "github3_utils.testing")
+try:
+	# 3rd party
+	import pytest_docker_tools  # type: ignore
+	pytest_plugins: Tuple[str, ...] = ("pytest_docker_tools", "coincidence", "github3_utils.testing")
+except ImportError:
+	pytest_plugins = ("coincidence", "github3_utils.testing")
 
 with Betamax.configure() as config:
 	config.cassette_library_dir = PathPlus(__file__).parent / "cassettes"
